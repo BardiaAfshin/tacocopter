67be50607ae191d0c46902a20bf0b1a7d4615d55
bardia.afshin <brandonusa@gmail.com>
2016-09-20 16:16:48 -0700

    Commit before going the nested attributes rails way.

--- app/controllers/tacofinder_controller.rb
+++ app/controllers/tacofinder_controller.rb
@@ -1,11 +1,23 @@
 class TacofinderController < ApplicationController
   def index
   	@tacos = Taco.all
-  	@taco = Taco.new
-
   	@salsas = Salsa.all
   end
 
   def search
+  	# TODO: implement permitted params
+  	# query we want to achieve via active record
+  	taco_list = params[:taco][:taco_name].map {|t| ActiveRecord::Base::sanitize(t) if t.length > 0 }.select{ |t| !t.nil? }.join(',')
+
+  	salsa_list = salsa_list = params[:salsa][:salsa_name].map {|t| ActiveRecord::Base::sanitize(t) if t.length > 0 }.select{ |t| !t.nil? }.join(',')
+  	
+
+  	query = "SELECT distinct(stores.name) as store_name, cities.name as city_name from tacos, stores_tacos, stores, cities, stores_salsas, salsas "\
+ 				 "WHERE tacos.id = stores_tacos.store_id AND stores.id = stores_tacos.store_id AND cities.id = stores.city_id AND stores.id = stores_salsas.store_id AND stores_salsas.salsa_id = salsas.id "\
+				  "AND LTRIM(RTRIM(tacos.name)) IN (" + taco_list +") " \
+				  "AND LTRIM(RTRIM(salsas.name)) IN ("+ salsa_list +")"
+
+
+	@results = ActiveRecord::Base.connection.execute(query)
   end
 end
